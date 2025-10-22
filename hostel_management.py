13f6184f1b9454c6f86ada532e192b3f3cf5c307
abdul-aziz <tamimaziz2007@gmail.com>
2025-10-22 07:50:23 +0600

    update

--- hostel_management.py
+++ hostel_management.py
@@ -32,34 +32,6 @@ def has_fixed_cost(roll):
             return True
     return False
 
-# ------------------- Tooltip Class -------------------
-# class ToolTip:
-#     def __init__(self, widget, text):
-#         self.widget = widget
-#         self.text = text
-#         self.tip_window = None
-#         self.widget.bind("<Enter>", self.show_tip)
-#         self.widget.bind("<Leave>", self.hide_tip)
-
-#     def show_tip(self, event=None):
-#         if self.tip_window or not self.text:
-#             return
-#         x, y, _, cy = self.widget.bbox("insert") or (0,0,0,0)
-#         x = x + self.widget.winfo_rootx() + 25
-#         y = y + cy + self.widget.winfo_rooty() + 20
-#         self.tip_window = tw = tk.Toplevel(self.widget)
-#         tw.wm_overrideredirect(True)
-#         tw.wm_geometry(f"+{x}+{y}")
-#         label = tk.Label(tw, text=self.text, justify='left',
-#                          background="#ffffe0", relief='solid', borderwidth=1,
-#                          font=("tahoma", "8", "normal"))
-#         label.pack(ipadx=5, ipady=3)
-
-#     def hide_tip(self, event=None):
-#         if self.tip_window:
-#             self.tip_window.destroy()
-#         self.tip_window = None
-
 # ------------------- Add Expense -------------------
 def add_expense():
     roll = roll_entry.get().strip()
